78d679b2947ea49086d3e83a0f23260b73abca93
FabienGuitton <fabienguittonfr@gmail.com>
2025-01-25 10:57:15 +0100

    Premier rendu des medias queries

--- index.js
+++ index.js
@@ -5,14 +5,4 @@
 
 bootstrap.Tooltip.VERSION // => "5.3.3"
 
-// Attendre que le DOM soit chargé avant l'initialisation
-document.addEventListener('DOMContentLoaded', () => {
-    const carouselElement = document.querySelector('#carousel1');
-    if (carouselElement) {
-        new bootstrap.Carousel(carouselElement, {
-            interval: 3000, // Temps entre les transitions
-            wrap: true, // Permet de boucler après la dernière slide
-            keyboard: true // Active la navigation avec le clavier
-        });
-    }
-});
+const carousel = new bootstrap.Carousel('#carousel1');
