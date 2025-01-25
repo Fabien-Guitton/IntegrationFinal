08bc852c1903703d5376a32c65e29203f1047de0
Evan <99046124+Evan-Beaufreton@users.noreply.github.com>
2025-01-25 15:26:44 +0100

    Update index.js
    copier le mauvais fichiers...

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const burgerMenu = document.getElementById("burger-menu");
-    const sideMenu = document.getElementById("side-menu");
-    const closeMenu = document.getElementById("close-menu");
+    const burgerMenu = document.getElementById("bouton_burger");
+    const menu_burger = document.getElementById("menu_burger");
+    const fermer = document.getElementById("fermer");
 
-    // Ouvre le menu latéral
     burgerMenu.addEventListener("click", () => {
-        sideMenu.style.left = "0";
+        menu_burger.style.left = "0";
     });
 
-    // Ferme le menu latéral
-    closeMenu.addEventListener("click", () => {
-        sideMenu.style.left = "-250px";
+    fermer.addEventListener("click", () => {
+        menu_burger.style.left = "-250px";
     });
 
-    // Fonction pour vérifier la taille de la fenêtre
     const checkWindowSize = () => {
         if (window.innerWidth > 1000) {
-            // Si la fenêtre est plus large que 1000px, on ferme le menu
-            sideMenu.style.left = "-250px";
+            menu_burger.style.left = "-250px";
         }
     };
 
-    // Écouteur d'événements pour la redimensionnement de la fenêtre
     window.addEventListener("resize", checkWindowSize);
 
-    // Vérifie la taille initiale de la fenêtre
     checkWindowSize();
 });
